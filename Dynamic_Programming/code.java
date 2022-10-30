2a0e3b25f22126ec61472caf2d4b4191378c1080
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-10-30 22:26:26 +0530

    Create code.java

---
+++ Dynamic_Programming/code.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+        int[] memo;
+public int rob(int[] nums) {
+    memo = new int[nums.length + 1];
+    Arrays.fill(memo, -1);
+    return rob(nums, nums.length - 1);
+}
+
+private int rob(int[] nums, int i) {
+    if (i < 0) {
+        return 0;
+    }
+    if (memo[i] >= 0) {
+        return memo[i];
+    }
+    int result = Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    memo[i] = result;
+    return result;
+}
+    
+}
