c8db32bb745e49ac3efbb7b414fa9fb95cae01ef
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-11-07 19:58:05 +0530

    Create code.cpp

---
+++ 268. Missing Number/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n+1,0);
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+        int ans;
+        for(int i=0;i<n+1;i++){
+            if(hash[i]==0){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
