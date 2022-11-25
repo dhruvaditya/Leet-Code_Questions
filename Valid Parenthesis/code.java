d33b72ff58c1b93e1124889f91ad7ae0286b7d08
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-11-25 23:40:00 +0530

    Create code.java

---
+++ Valid Parenthesis/code.java
@@ -0,0 +1,17 @@
+class Solution {
+  // debug this code and try to understand this algorithm deeply
+   public boolean isValid(String s) {
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+}
+}
