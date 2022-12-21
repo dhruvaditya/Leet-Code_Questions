c0d1f470f295e7248ffaaf36b232516ea2b376c5
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-12-21 23:58:41 +0530

    Create code.cpp

---
+++ Power of Four/code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        else if(n==0){
+            return false;
+        }
+        while(n%4==0){
+            n=n/4;
+        }
+        if(n==1){
+            return true;
+        }
+        else 
+        return false;
+    }
+};
