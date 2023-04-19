b1f0dff36b547384b1282e8bf7b2aa32f7bdc81b
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2023-04-19 22:52:48 +0530

    Create code.cpp

---
+++ LongestZigZagPathInABinaryTree/code.cpp
@@ -0,0 +1,25 @@
+ */
+class Solution {
+public:
+int pathlength=0;
+    void dfs(TreeNode *node, bool goleft, int steps){
+        if(node ==NULL){
+            return;
+        }
+        pathlength=max(pathlength,steps);
+        if(goleft){
+            dfs(node->left,false,steps+1);
+            dfs(node->right,true,steps);
+        }
+        else{
+            dfs(node->left,false,steps);
+            dfs(node->right,true,steps+1);
+        }
+    }
+    int longestZigZag(TreeNode* root) {
+        dfs(root,false,0);
+        dfs(root,true,0);
+        return pathlength;
+        
+    }
+};
