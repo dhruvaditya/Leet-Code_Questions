3a4074a8709c0f41df3985a916350e6ce347b0e2
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2023-05-15 22:46:38 +0530

    Create code.cpp

---
+++ Swapping linkedlist Nodes/code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+     ListNode fast=head,slow=head,first=head,second=head;
+     for(int i=0;i<k-1;++i){
+         fast=fast.next;
+     }
+     first=fast;
+     while(fast.next!=null){
+         slow=slow.next;
+         fast=fast.next;
+     }
+     second=slow;
+     int temp=first.val;
+     first.val=second.val;
+     second.val=temp;  
+        
+     return head;   
+    }
+}
